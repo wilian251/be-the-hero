13f43e915612a25496f9ec3e973ab5240f806ff6
Wilian Ribeiro <wilian.ribeiro@itspoa.com>
2020-04-17 14:19:48 -0300

    front-end do app

--- backend/src/controllers/IncidentsController.js
+++ backend/src/controllers/IncidentsController.js
@@ -35,7 +35,7 @@ module.exports = {
 
     async delete(request, response){
         const { id } = request.params;
-        const ong_id = request.header.authorization;
+        const ong_id = request.headers.authorization;
 
         const incident = await connection('incidents')
         .where('id', id)
